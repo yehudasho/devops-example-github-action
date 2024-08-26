6ead5fcd1d10c1fec8e2ed4183695e54e9609001
yehudasho <55053156+yehudasho@users.noreply.github.com>
2024-08-26 15:03:51 +0300

    Update app.js

--- base/app.js
+++ base/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Testing Github action !!!\n');
+  res.end('Testing Github action-01 !!!\n');
 });
 
 server.listen(port, hostname, () => {
